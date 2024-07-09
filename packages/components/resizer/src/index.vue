37ff53837a251adf5f3707d0194d9f7c02ff8355
jiantao.xiu@ytever.com <jiantao.xiu@ytever.com>
2024-07-09 10:47:52 +0800

    fix: 新增拖拽改变大小组件

---
+++ packages/components/resizer/src/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <el-switch class="btp-switch" ref="switchRef">
+        <template #active-action><slot name="active-action" /></template>
+        <template #inactive-action>
+            <slot name="inactive-action" />
+        </template>
+    </el-switch>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const switchRef = ref()
+
+/**
+ * 将ElementPlus的内置方法继续暴露出去
+ */
+defineExpose({
+    focus: () => {
+        switchRef.value.focus()
+    },
+})
+</script>
