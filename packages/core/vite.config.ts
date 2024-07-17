64722b059f02837d2ee28ff1241accc04b7a1a4d
于智勇 <zhiyong.yu@ytever.com>
2024-07-17 09:05:09 +0800

    fix: core包可以分包构建；dts文件暂未生成

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -2,15 +2,59 @@
 import { defineConfig } from 'vite'
 import baseConfig from '../../vite.config'
 import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
     ...baseConfig,
     build: {
-        ...baseConfig.build,
+        minify: false,
+        rollupOptions: {
+            external: id => {
+                const externals = [
+                    'vue',
+                    'lodash',
+                    'lodash-es',
+                    'vue-router',
+                    '@vueuse/core',
+                    'axios',
+                    'element-plus',
+                    'nprogress',
+                    'vue-demi',
+                    '@vue_shared',
+                    'vue-cookies',
+                    'pinia',
+                    'uuid',
+                ]
+                // 匹配直接模块名
+                if (externals.includes(id)) {
+                    return true
+                }
+                // 匹配子模块情况，如 nprogress 的子路径
+                return externals.some(pkg => id.startsWith(pkg))
+            },
+            input: ['index.ts'],
+            output: [
+                {
+                    format: 'es',
+                    entryFileNames: '[name].js',
+                    preserveModules: true,
+                    exports: 'named',
+                    dir: './dist/es',
+                },
+                {
+                    format: 'cjs',
+                    entryFileNames: '[name].js',
+                    preserveModules: true,
+                    exports: 'named',
+                    dir: './dist/lib',
+                },
+            ],
+        },
         lib: {
-            entry: resolve(__dirname, './index.ts'),
-            name: 'Core',
-            fileName: format => `core.${format}.js`,
+            entry: './index.ts',
+            formats: ['es', 'cjs'],
         },
     },
+    plugins: [vue()],
 })
